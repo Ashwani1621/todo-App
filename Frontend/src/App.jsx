625dab458f9ba77ac8fe00abf144245e0b5bd25b
Ashwani <itsashksingh10@gmail.com>
2025-02-11 20:51:07 +0530

    App v1.0

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,68 @@
 import { useState } from 'react'
 
 import './App.css'
+function TodoApp(){
 
+  const [list, setList] = useState([{title:"Task", desc:"Description"}])
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+
+  return(
+    <>
+      <input type="text" placeholder="Enter task" value={title} onChange={handleTitle}/>
+      <input type="text" placeholder="Enter desc" value={desc} onChange={handleDesc}/>
+      <br />
+      <button onClick={()=>{
+        addTodo()
+      }}>Add Task</button>
+    <button onClick={()=>{
+      resetTodo()
+    }}>Reset</button>
+        <ol className="list">
+          {list.map((value, index)=>(
+        <li className="list-item" key={index}> 
+          <h1>{value.title}</h1>
+          <h4>{value.desc}</h4>
+          <button onClick={()=>{
+            removeTodo(index)
+            }}>Remove</button>
+        </li>
+       ))}
+      </ol>
+    </>
+  )
+  function handleTitle(e){
+    const title = e.target.value;
+    setTitle(title)
+  }
+  function handleDesc(e){
+    const desc = e.target.value;
+    setDesc(desc)
+  }
+  function addTodo(){
+    setList([...list, {title, desc}])
+    setDesc("")
+    setTitle("")
+  }
+  function removeTodo(k){
+    setList(list.filter((_ , index)=>(
+      index!==k
+    )))
+  }
+  function resetTodo(){
+    setList([])
+    setDesc("")
+    setTitle("")
+  }
+}
+function App(){
+  return(
+    <>
+     
+      <TodoApp/>
+    </>
+  )
+}
 
 
 export default App
